ba729f6fd01031ec9ead92316b6d17cca96854ed
Edy Segura <edysegura@gmail.com>
2018-09-26 23:46:23 -0300

    Refactoring to increase the code readability

--- sockets/server.js
+++ sockets/server.js
@@ -1,16 +1,21 @@
 const net = require('net')
 
+const port = 4000
+const address = '127.0.0.1'
+
+const handleDataStream = socket => data => {
+  const text = String(data)
+  if (text === 'xispa') {
+    socket.end()
+  }
+  console.log(text)
+}
+
 const handleConnection = socket => {
   console.log('Someone just connected!')
   socket.on('end', () => console.log('Desconnected'))
-  socket.on('data', data => {
-    const text = data.toString()
-    if (text === 'xispa') {
-      socket.end()
-    }
-    console.log(data.toString())
-  })
+  socket.on('data', handleDataStream(socket))
 }
 
-const server = net.createServer(handleConnection)
-server.listen(4000, '127.0.0.1')
+net.createServer(handleConnection)
+  .listen(port, address)
