d74f1c81b2ddf547d182403ad14e143ea05faebc
Edy Segura <edysegura@gmail.com>
2018-10-18 18:03:23 -0300

    Added EcmaScript Module example

---
+++ es-module/index.mjs
@@ -0,0 +1,8 @@
+import os from 'os'
+
+console.log('Platform: ', os.platform())
+console.log('Release: ', os.release())
+console.log('CPU Architecutre: ', os.arch())
+console.log('CPUs: ', os.cpus().length)
+console.log('Hostmanme: ', os.hostname())
+console.log('User Info: ', os.userInfo())
