d0c53ecfa2903d82f072e83b4fff57de344393e3
Edy Segura <edysegura@gmail.com>
2020-06-30 16:37:00 -0300

    Refactor bcryptjs example

--- bcryptjs/index.js
+++ bcryptjs/index.js
@@ -2,7 +2,7 @@ import bcryptjs from 'bcryptjs'
 
 async function main() {
   const salt = 8
-  const generatedHash = await bcryptjs.hash('batatinha', salt)
+  const generatedHash = await bcryptjs.hash('your secret password', salt)
   console.log('Hash: ', generatedHash)
 }
 
