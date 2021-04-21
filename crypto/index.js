44629f1bfe302519733a493ada1d3c5977f889ca
Edy Segura <edysegura@gmail.com>
2021-04-21 20:18:39 -0300

    Update crypto example

--- crypto/index.js
+++ crypto/index.js
@@ -2,5 +2,8 @@ import crypto from 'crypto'
 
 // Password-Based Key Derivation Function 2
 crypto.pbkdf2('secret', 'salt', 100000, 32, 'sha512', (error, derivedKey) => {
-  console.log(derivedKey.toString('hex'))
+  console.log('1', derivedKey.toString('hex'))
 })
+
+const hash = crypto.createHash('sha256').update('secret').digest('hex')
+console.log('2', hash)
