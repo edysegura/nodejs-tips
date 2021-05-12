39488b0c965530e232db6dd0085fa3557d82926b
Edy Segura <edysegura@gmail.com>
2021-05-11 21:59:32 -0300

    Add cluster example

---
+++ cluster/index.js
@@ -0,0 +1,10 @@
+import cluster from 'cluster'
+
+if (cluster.isMaster) {
+  console.log('Master node is running...')
+  cluster.fork()
+}
+else {
+  console.log('Child node is running...')
+  console.log('Press CTRL+C to finalize')
+}
