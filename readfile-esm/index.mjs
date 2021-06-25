4a2647efea47b5814e657170884d0b7c0c97582a
Edy Segura <ednardo.segura@mundiale.com.br>
2021-06-24 21:32:15 -0300

    Add an example to load JSON file using ESM
    This closes #25
    This closes #24

--- readfile-esm/index.mjs
+++ readfile-esm/index.mjs
@@ -4,3 +4,11 @@ const csvPath = new URL('./contacts.csv', import.meta.url)
 const csvText = await readFile(csvPath, 'utf-8')
 
 console.log(csvText)
+
+// --------------------------------
+
+const jsonPath = new URL('./contacts.json', import.meta.url)
+const jsonText = await readFile(jsonPath)
+const jsonData = JSON.parse(jsonText)
+
+console.table(jsonData)
