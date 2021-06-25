0bc47be48c25540be6972bf7632d64f224cb0de3
Edy Segura <edysegura@gmail.com>
2021-06-25 20:13:58 -0300

    Implement axios retry example

---
+++ axios-retry/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import axiosRetry from 'axios-retry'
+
+axiosRetry(axios, {
+  retries: 3,
+  retryDelay: (retryCount) => {
+    console.log(`retry attempt: ${retryCount}`)
+    return retryCount * 2000
+  },
+  retryCondition: (error) => {
+    return error.response.status === 503
+  }
+})
+
+try {
+  const response = await axios.get('https://httpstat.us/503')
+} catch (error) {
+  console.log(error.message)
+}
