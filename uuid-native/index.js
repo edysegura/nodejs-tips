b9e61ad69ed9252ef588726d1ee4333135aa9f34
Edy Segura <edysegura@gmail.com>
2021-10-27 22:56:09 -0300

    refactor: generate 4 uuid instead of 1

--- uuid-native/index.js
+++ uuid-native/index.js
@@ -1,3 +1,10 @@
 const { randomUUID } = require('crypto'); // Added in: node v14.17.0
 
-console.log(randomUUID()); // 35867639-20b2-4b76-9557-bdae6ac0ac14
+[
+  randomUUID(),
+  randomUUID(),
+  randomUUID(),
+  randomUUID(),
+].forEach(uuid => console.log(uuid))
+
+// crypto.randomUUID is three times faster than uuid. And no need to add extra dependency.
