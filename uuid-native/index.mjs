f5db9d4b108289f48cc046ccd8a9e3d573f4f6f8
Edy Segura <edysegura@gmail.com>
2021-10-29 21:48:43 -0300

    feat: add uuid native example with esm

---
+++ uuid-native/index.mjs
@@ -0,0 +1,10 @@
+import { randomUUID } from  'crypto' // Added in: node v14.17.0
+
+[
+  randomUUID(),
+  randomUUID(),
+  randomUUID(),
+  randomUUID(),
+].forEach(uuid => console.log(uuid))
+
+// crypto.randomUUID is three times faster than uuid. And no need to add extra dependency.
