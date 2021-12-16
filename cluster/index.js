3d0ba0ff6f7aaf390c4915ab2710f79833c3c8d0
Edy Segura <edysegura@gmail.com>
2021-12-15 23:19:04 -0300

    code formatting

--- cluster/index.js
+++ cluster/index.js
@@ -3,8 +3,7 @@ import cluster from 'cluster'
 if (cluster.isMaster) {
   console.log('Master node is running...')
   cluster.fork()
-}
-else {
+} else {
   console.log('Child node is running...')
   console.log('Press CTRL+C to finalize')
 }
