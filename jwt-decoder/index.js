fd5233761f6df2600f0249d406ee00d14264d2ae
Edy Segura <edysegura@gmail.com>
2022-04-05 10:28:16 -0300

    style: prettierfy

--- jwt-decoder/index.js
+++ jwt-decoder/index.js
@@ -1,5 +1,6 @@
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-const [,tokenPayload] = token.split('.')
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+const [, tokenPayload] = token.split('.')
 const parsedPayload = Buffer.from(tokenPayload, 'base64').toString()
 
 console.log(parsedPayload) // {"sub":"1234567890","name":"John Doe","iat":1516239022}
