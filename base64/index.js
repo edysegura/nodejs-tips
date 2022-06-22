874ff251337f44ddc81f468395b4a5f8e1a07da8
Edy Segura <edysegura@gmail.com>
2022-06-22 08:59:23 -0300

    feat: add string length for comparison

--- base64/index.js
+++ base64/index.js
@@ -1,5 +1,5 @@
 const encodeBase64 = Buffer.from('someone@email.com').toString('base64')
-console.log({ encodeBase64 })
+console.log({ encodeBase64, length: encodeBase64.length })
 
 const decodeBase64 = Buffer.from(encodeBase64, 'base64').toString('utf-8')
-console.log({ decodeBase64 })
+console.log({ decodeBase64, length: decodeBase64.length })
