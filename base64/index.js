bf7da506c40ed7a5de3335fb3546a871f8e528a2
Edy Segura <edysegura@gmail.com>
2022-06-21 22:07:06 -0300

    feat: add decode base64 example

--- base64/index.js
+++ base64/index.js
@@ -1,2 +1,5 @@
 const base64 = Buffer.from('someone@email.com').toString('base64')
 console.log({ base64 })
+
+const decodeBase64 = Buffer.from(base64, 'base64').toString('utf-8')
+console.log({ decodeBase64 })
