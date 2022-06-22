4762da46e2dab743e452363a65a38b70ab7d6745
Edy Segura <edysegura@gmail.com>
2022-06-21 22:08:50 -0300

    refactor: base64 example

--- base64/index.js
+++ base64/index.js
@@ -1,5 +1,5 @@
-const base64 = Buffer.from('someone@email.com').toString('base64')
-console.log({ base64 })
+const encodeBase64 = Buffer.from('someone@email.com').toString('base64')
+console.log({ encodeBase64 })
 
-const decodeBase64 = Buffer.from(base64, 'base64').toString('utf-8')
+const decodeBase64 = Buffer.from(encodeBase64, 'base64').toString('utf-8')
 console.log({ decodeBase64 })
