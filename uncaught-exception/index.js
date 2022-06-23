a1475f56a941b36a8d9fe532b054f5eaeb024e42
Edy Segura <edysegura@gmail.com>
2022-06-22 23:08:44 -0300

    chore: prettierfy

--- uncaught-exception/index.js
+++ uncaught-exception/index.js
@@ -1,6 +1,6 @@
 process.on('unhandledRejection', (reason, promise) => {
   console.log(
-    `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`
+    `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`,
   )
   // lets throw the error and let the uncaughtException handle below handle it
   throw reason
@@ -11,13 +11,11 @@ process.on('uncaughtException', (error) => {
   process.exit(1)
 })
 
-
 function asyncAction() {
   return new Promise((_, reject) => {
     reject('Error from promise')
   })
 }
-
 
 // x.y
 
