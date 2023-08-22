2eb2aae7ac32d855167f26d845b895e9780c1ea6
Edy Segura <edysegura@gmail.com>
2023-08-21 23:04:20 -0300

    chore: added example to cipher/decipher message

---
+++ crypto/cipher-decipher-message.mjs
@@ -0,0 +1,33 @@
+import crypto from 'crypto'
+
+const algorithm = 'aes-256-cbc'
+
+// generate 16 bytes of random data
+const initVector = crypto.randomBytes(16)
+
+// protected data
+const message = 'This is a secret message'
+
+// secret key generate 32 bytes of random data
+const secretKey = crypto.randomBytes(32)
+
+// the cipher function
+const cipher = crypto.createCipheriv(algorithm, secretKey, initVector)
+
+// encrypt the message
+// input encoding
+// output encoding
+let encryptedData = cipher.update(message, 'utf-8', 'hex')
+
+encryptedData += cipher.final('hex')
+
+console.log('Encrypted message: ' + encryptedData)
+
+// the decipher function
+const decipher = crypto.createDecipheriv(algorithm, secretKey, initVector)
+
+let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8')
+
+decryptedData += decipher.final('utf8')
+
+console.log('Decrypted message: ' + decryptedData)
