e726d9da02b6fd686617c05589a662af91508253
Edy Segura <edysegura@gmail.com>
2023-09-07 16:26:28 -0300

    refactor: true random values example

--- crypto/true-random-numbers.mjs
+++ crypto/true-random-numbers.mjs
@@ -3,7 +3,8 @@ import { getRandomValues } from 'crypto' // dropped on node v14.17.0
 const typedArray = new Uint32Array(10)
 getRandomValues(typedArray)
 
+const printNumber = (number, i) =>
+  console.log(`${String(i + 1).padStart(2, '0')} - ${number}`)
+
 console.log('Your lucky numbers:')
-for (const num of typedArray) {
-  console.log(num)
-}
+typedArray.forEach(printNumber)
