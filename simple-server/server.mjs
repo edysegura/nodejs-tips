9f2e8210d6007afd220fe40e8a4f11387f32f444
Edy Segura <edysegura@gmail.com>
2024-12-26 23:08:56 -0300

    feat: add a simple server example

---
+++ simple-server/server.mjs
@@ -0,0 +1,22 @@
+// server.mjs
+import { createServer } from 'node:http'
+import { readFile } from 'node:fs/promises'
+
+const server = createServer(async (req, res) => {
+  try {
+    const data = await readFile('./index.html', 'utf8')
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(data)
+  } catch (err) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.end('Internal Server Error\n')
+  }
+})
+
+// starts a simple http server locally on port 3000
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
+})
+
+// supported since Node.js v14+
+// run with `node server.mjs`
