c019fcc9cd91d9ab6b36f7e92fe00573ac61c2bb
Edy Segura <edysegura@gmail.com>
2025-01-24 19:58:02 -0300

    refactor: node.js native test runner

---
+++ native-test-runner/index.test.mjs
@@ -0,0 +1,11 @@
+import assert from 'node:assert'
+import { describe, it } from 'node:test'
+
+describe('Test suite', () => {
+  it('that 1 is equal 1', () => {
+    assert.strictEqual(1, 1)
+  })
+})
+
+// run with `node --test` or `node --test --watch`
+// run with `node index.test.mjs` or `node index.test.mjs --watch`
