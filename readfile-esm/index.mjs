feb7abd5332b8e2daec522e6fe364e07bfdcdd4e
Edy Segura <edysegura@gmail.com>
2025-06-30 18:47:45 -0300

    fix: update import path for readFile to use 'node:fs/promises'

--- readfile-esm/index.mjs
+++ readfile-esm/index.mjs
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises'
+import { readFile } from 'node:fs/promises'
 
 const csvPath = new URL('./contacts.csv', import.meta.url)
 const csvText = await readFile(csvPath, 'utf-8')
